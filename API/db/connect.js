ece5ecaa939a9b60fa5c08e8e1da0785e00f7c8d
Raghav-0311 <raghavkashyap0311@gmail.com>
2023-12-18 04:52:42 +0530

    Tested DB Connecction ✅

--- API/db/connect.js
+++ API/db/connect.js
@@ -4,7 +4,11 @@ import 'dotenv/config.js';
 const URI = process.env.DB_CONNECTION;
 
 const connectDB = () => {
-    return mongoose.connect(URI);
+    return mongoose
+        .connect(URI)
+        .then(() => {
+            console.log("Successfully Connected to Database ✅");
+        });
 }
 
 export default connectDB
